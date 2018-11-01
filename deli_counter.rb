003b02e945ae2515fff46e6492766533b8717826
Ryan Gill <rdgill95@gmail.com>
2018-11-01 19:02:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli)
-  katz_deli <<   
+  katz_deli << katz_deli.length +1   
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
